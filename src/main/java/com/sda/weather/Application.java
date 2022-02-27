4c7122ba2880fcc9e53d84419688d05fd8b3ddf0
Michal Paukszto <michal.paukszto@gmail.com>
2022-02-27 14:29:15 +0100

    merge test

--- src/main/java/com/sda/weather/Application.java
+++ src/main/java/com/sda/weather/Application.java
@@ -3,6 +3,7 @@ package com.sda.weather;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("Michal tu byl");
         System.out.println("Hello world");
     }
 }
